02b965cb5f66ce745db227f21999eed215b0be83
rariasnav <137901995+rariasnav@users.noreply.github.com>
2024-05-26 17:25:46 +0000

    commit

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,161 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
+class Roles(enum.Enum):
+    user = 'user'
+    admin = 'admin'
+
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    user_name = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    role = db.Column(db.Enum(Roles), unique=False, nullable=False)
+    # likes = db.relationship('Likes', backref='user', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.user_name}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_name": self.user_name,
+            "role": self.role.name
+        }
+    
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    heigth	= db.Column(db.Integer, unique=False, nullable=False)
+    mass = db.Column(db.Integer, unique=False, nullable=False)
+    hair_color = db.Column(db.String(40), unique=False, nullable=False)
+    skin_color = db.Column(db.String(40), unique=False, nullable=False)
+    eye_color = db.Column(db.String(40), unique=False, nullable=False)
+    birth_year = db.Column(db.String(20), unique=False, nullable=False)
+    gender = db.Column(db.String(10), unique=False, nullable=False)
+    likes = db.relationship('Likes', backref='people', lazy=True)
+
+    def __repr__(self):
+        return f'<People {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "heigth": self.heigth,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender
+        }
+    
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    rotation_period	= db.Column(db.Integer, unique=False, nullable=False)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
+    climate	= db.Column(db.String(40), unique=False, nullable=False)
+    gravity	= db.Column(db.String(20), unique=False, nullable=False)
+    terrain	= db.Column(db.String(40), unique=False, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    likes = db.relationship('Likes', backref='planets', lazy=True)
+
+    def __repr__(self):
+        return f'<People {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "population": self.population
+        }
+    
+class Starships(db.Model):
+    __tablename__ = 'starships'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    model = db.Column(db.String(80), unique=False, nullable=False)
+    manufacturer = db.Column(db.String(40), unique=False, nullable=False)
+    cost_in_credits = db.Column(db.Integer, unique=False, nullable=False)
+    length = db.Column(db.Integer, unique=False, nullable=False)
+    max_atmosphering_speed = db.Column(db.Integer, unique=False, nullable=False)
+    crew = db.Column(db.Integer, unique=False, nullable=False)
+    passengers = db.Column(db.Integer, unique=False, nullable=False)
+    cargo_capacity = db.Column(db.Integer, unique=False, nullable=False)
+    consumables = db.Column(db.String(30), unique=False, nullable=False)
+    hyperdrive_rating = db.Column(db.Integer, unique=False, nullable=False)
+    MGLT =  db.Column(db.Integer, unique=False, nullable=False)
+    starship_class = db.Column(db.String(60), unique=False, nullable=False)
+    likes = db.relationship('Likes', backref='starships', lazy=True)
+
+    def __repr__(self):
+        return f'<Starships {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "hyperdrive_rating": self.hyperdrive_rating,
+            "MGLT": self.MGLT,
+            "starship_class": self.starship_class
+        }
+    
+class Likes(db.Model):
+    __tablename__ = 'likes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+    starship_id = db.Column(db.Integer, db.ForeignKey('starships.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Likes {self.id}>'
+    
+    def serialize(self):
+
+        user = User.query.get(self.user_id)
+        if user is not None:
+            user = user.serialize()
+
+        person = People.query.get(self.person_id)
+        if person is not None:
+            person = person.serialize()
+
+        planet = PLanets.query.get(self.planet_id)
+        if planet is not None:
+            planet = planet.serialize()
+
+        starship = Starships.query.get(self.starship_id)
+        if starship is not None:
+            starship = starship.serialize()
+
+        return {
+            "id": self.id,
+            "user": user,
+            "person": person,
+            "planet": planet,
+            "starship": starship,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,10 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from flask import Flask, request, jsonify, url_for, Blueprint, current_app
+from api.models import db, User, Roles, People, Planets, Starships, Likes
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,174 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    user = User.query.filter_by(user_name=body['user_name']).first()
+
+    if user != None:
+        return jsonify({"msg": "Username already in use"}), 400
+
+    if user is None:
+        password_hash = current_app.bcrypt.generate_password_hash(body['password']).decode('utf-8')
+        new_user = User(
+            user_name = body['user_name'],
+            password = password_hash,
+            role = 'user',
+            is_active = True
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"msg": "User created"}), 200
+    
+@api.route('/login', methods=['POST'])
+def login():
+    user_name = request.json.get('user_name', None)
+    password = request.json.get('password', None)
+    user = User.query.filter_by(user_name=user_name).first()
+
+    if user is None:
+        return jsonify({"msg": "Username not found"}), 401
+    
+    decrypted_password = current_app.bcrypt.check_password_hash(user.password, password)
+
+    if user_name != user.user_name or decrypted_password is False:
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=user_name)
+    return jsonify(user=user.serialize(), access_token=access_token), 200
+
+@api.route('/profile', methods=['GET'])
+@jwt_required()
+def get_in_profile():
+    user_name = get_jwt_identity()
+    user = User.query.filter_by(user_name=user_name).first()
+
+    response_body = {
+        "msg": "Username found",
+        "User": user.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/people', methods=['POST'])
+@jwt_required()
+def create_person():
+    user_name = get_jwt_identity()
+    user = User.query.filter_by(user_name=user_name).first()
+    body = request.get_json()
+    person = People.query.filter_by(name=body['name']).first()
+
+    if user is None:
+        return jsonify({"msg": "User not found"}), 404
+    
+    if person is not None:
+        return jsonify({"msg": "Person already in system"}), 401
+
+    if user.role == Roles.admin:
+        new_person = People()
+        for key, value in body.items():                
+            setattr(new_person, key, value)
+
+        db.session.add(new_person)
+        db.session.commit()                    
+
+        return jsonify({"msg": "Person created"}), 201
+    
+    else:
+        return jsonify({"msg": "Not allowed"}), 403
+    
+@api.route('/planets', methods=['POST'])
+@jwt_required()
+def create_planet():
+    user_name = get_jwt_identity()
+    user = User.query.filter_by(user_name=user_name).first()
+    body = request.get_json()
+    planet = Planets.query.filter_by(name=body['name']).first()
+
+    if user is None:
+        return jsonify({"msg": "User not found"}), 404
+    
+    if planet is not None:
+        return jsonify({"msg": "Planet already in system"}), 401
+    
+    if user.role == Roles.admin:
+        new_planet = Planets()
+        for key, value in body.items():
+            setattr(new_planet, key, value)
+
+        db.session.add(new_planet)
+        db.session.commit()
+    
+        return jsonify({"msg": "Planet created"}), 201
+    else:
+        return jsonify({"msg": "Not allowed"}), 403
+
+@api.route('/starships', methods=['POST'])
+@jwt_required()
+def create_starship():
+    user_name = get_jwt_identity()
+    user = User.query.filter_by(user_name=user_name).first()
+    body = request.get_json()
+    starship = Starships.query.filter_by(name=body['name']).first()
+
+    if user is None:
+        return jsonify({"msg": "User not found"}), 404
+    
+    if starship is not None: 
+        return jsonify({"msg": "Starship already in system"}), 401
+    
+    if user.role == Roles.admin:
+        new_starship = Starships()
+        for key, value in body.items():
+            setattr(new_starship, key, value)
+
+        db.session.add(new_starship)
+        db.session.commit()
+
+        return jsonify({"msg": "Starship created"}), 201
+    
+    else:
+        return jsonify({"msg": "Not allowed"}), 403
+
+@api.route('/people', methods=['GET'])
+def get_all_people():
+    people = People.query.all()
+    result = list(map(lambda person: person.serialize() ,people))
+
+    return jsonify(result), 200
+
+@api.route('/people/<person_id>', methods=['GET'])
+def get_person(person_id):
+    person = People.query.filter_by(id=person_id).first()
+
+    return jsonify(person.serialize()), 200
+
+@api.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets = Planets.query.all()
+    result = list(map(lambda planet: planet.serialize(), planets))
+
+    return jsonify(result), 200
+
+@api.route('/planets/<planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planets.query.filter_by(id=planet_id).first()
+
+    return jsonify(planet.serialize()), 200
+
+@api.route('/starships', methods=['GET'])
+def get_all_starships():
+    starships = Starships.query.all()
+    result = list(map(lambda starship: starship.serialize(), starships))
+
+    return jsonify(result), 200
+
+@api.route('/starships/<starship_id>', methods=['GET'])
+def get_starship(starship_id):
+    starship = Starships.query.filter_by(id=starship_id).first()
+
+    return jsonify(starship.serialize()), 200
